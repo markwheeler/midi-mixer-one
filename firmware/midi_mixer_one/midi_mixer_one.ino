369904d8c60226f7d53d98b493a78c62708dfccd
Mark Wheeler <mark@markwheeler.net>
2020-08-15 21:25:42 -0700

    formatting, param tweaks, pot res, all pots enabled

--- firmware/midi_mixer_one/midi_mixer_one.ino
+++ firmware/midi_mixer_one/midi_mixer_one.ino
@@ -7,7 +7,8 @@
 
 #define MIDI_CHANNEL 1
 
-const int MIDI_CCS[] = { // For pots
+// Pot CCs
+const int MIDI_CCS[] = {
   2, 3, 4, 5, 6,
   7, 8, 9, 10, 11,
   12, 13, 14, 15, 16,
@@ -18,20 +19,22 @@ const int MIDI_CCS[] = { // For pots
   37, 38, 39, 40, 41,
   42
   };
-  
-const int MIDI_NOTE_NUMS[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; // For switches
 
+// Switch note numbers
+const int MIDI_NOTE_NUMS[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+// Define a key that sends all pot values (-1 to disable)
 #define SEND_ALL_KEY 8
 
 /////////////////////
 
 
-// Pots
+// Pot params
 #define POT_BIT_RES             10 // 7-16 is valid
-#define POT_NUM_READS           32 // 32 works
+#define POT_NUM_READS           4 // TODO test - 32 works
 #include <ResponsiveAnalogRead.h>
 
-// Switches
+// Switch params
 #define BOUNCE_LOCK_OUT
 #define SWITCH_INTERVAL         5 // ms
 #include <Bounce2.h>
@@ -43,17 +46,17 @@ const int MIDI_NOTE_NUMS[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; // For 
 #define NUM_MUX_CHANNELS        8
 #define NUM_SWITCHES            12
 
-const int MUX_PINS[] = {14, 15, 16, 17, 18};
-const int MUX_SIGNAL_PINS[] = {23, 22, 21};
-const int MUX_POTS_BY_CHANNEL[][8] = {
+const int MUX_PINS[] =          {14, 15, 16, 17, 18};
+const int MUX_SIGNAL_PINS[] =   {23, 22, 21};
+const int MUX_POTS_BY_CHANNEL[][NUM_MUX_CHANNELS] = {
   {1, 2, 3, 0, 4, 5, 7, 6},
   {11, 8, 9, 10, 14, 15, 13, 12},
   {17, 18, 19, 16, 23, 20, 22, 21},
   {24, 28, 29, 27, 31, 26, 30, 25},
   {33, 34, 39, 32, 38, 35, 37, 36},
 };
-const int MASTER_POT_PIN = 19;
-const int SWITCH_PINS[] = {3, 11, 7, 2, 6, 8, 1, 5, 9, 0, 4, 10};
+#define MASTER_POT_PIN          19
+const int SWITCH_PINS[] =       {3, 11, 7, 2, 6, 8, 1, 5, 9, 0, 4, 10};
 
 // Vars
 ResponsiveAnalogRead pots[NUM_POTS];
@@ -63,10 +66,14 @@ int pot_midi_values[NUM_POTS];
 
 void setup() {
   Serial.begin(38400);
+  
+  analogReadResolution(POT_BIT_RES);
+  analogReadAveraging(POT_NUM_READS);
 
   // Pots
   for(int i = 0; i < NUM_POTS; i ++) {
     pots[i] = ResponsiveAnalogRead(0, true); // TODO consider options here
+    pots[i].setAnalogResolution(pow(2, POT_BIT_RES));
   }
   
   for(int i = 0; i < NUM_MUX_SIGNAL_PINS; i ++) {
@@ -102,7 +109,7 @@ void loop() {
     for(int s = 0; s < NUM_MUX_SIGNAL_PINS; s ++) {
       digitalWrite(MUX_SIGNAL_PINS[s], bitRead(c, s));
     }
-    delayMicroseconds(50);
+    delayMicroseconds(50); // TODO tweak
 
     // Read from all muxes
     for(int m = 0; m < NUM_MUXES; m ++) {
@@ -117,16 +124,13 @@ void loop() {
   // Check values
   for(int i = 0; i < NUM_POTS; i ++) {
     if(pots[i].hasChanged()) {
-
+      
       int new_midi_value = pots[i].getValue() >> (POT_BIT_RES - 7);
-
       if(new_midi_value != pot_midi_values[i]) {
-        if(i == 32) {
-          Serial.print(i);
-          Serial.print(" changed to " );
-          Serial.println(new_midi_value);
-          usbMIDI.sendControlChange(MIDI_CCS[i], new_midi_value, MIDI_CHANNEL);
-        }
+        Serial.print(i);
+        Serial.print(" changed to " );
+        Serial.println(new_midi_value);
+        usbMIDI.sendControlChange(MIDI_CCS[i], new_midi_value, MIDI_CHANNEL);
         pot_midi_values[i] = new_midi_value;
       }
     }
@@ -165,6 +169,6 @@ void loop() {
   while (usbMIDI.read()) {
   }
   
-  delay(10); //TODO experiment!
+  delay(10); // ms TODO experiment! Two refs have no delay, tehn's has 4ms
 
 }
