9becc82a8cd30aa25db95caa63e655ebd8a8d51b
Mark Wheeler <mark@markwheeler.net>
2021-05-10 20:47:24 -0700

    updated remote script for live 11

--- ableton_live_remote_script/Mixer_One/__init__.py
+++ ableton_live_remote_script/Mixer_One/__init__.py
@@ -1,3 +1,9 @@
-from MixerOne import MixerOne
+import _Framework.Capabilities as caps
+from .MixerOne import MixerOne
+
+def get_capabilities():
+    return {SUGGESTED_PORT_NAMES_KEY: [u'Mixer One - DAW Control'],
+    PORTS_KEY: [inport(props=[NOTES_CC, SCRIPT, REMOTE]), outport(props=[NOTES_CC, SCRIPT, REMOTE])]}
+
 def create_instance(c_instance):
     return MixerOne(c_instance)
